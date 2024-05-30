565bff3fc477303ca4381f26db20d9c8b313d139
LOSOcb <m.ilyshin@yandex.ru>
2024-05-30 22:08:22 +0300

    Initial commit

---
+++ app/api/config.js
@@ -0,0 +1,7 @@
+export const BASE_URL = 'https://finalbeck.nomorepartiesco.ru/admin/dashboard'
+
+export const endpoints = {
+  games: `${BASE_URL}/games`,
+  auth: `${BASE_URL}/auth/local`,
+  me: `${BASE_URL}/users/me`,
+}
