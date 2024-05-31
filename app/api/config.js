efae2bca7a502947ad2edfeb76d84cc62e55f046
LOSOcb <m.ilyshin@yandex.ru>
2024-05-31 22:20:45 +0300

    -

--- app/api/config.js
+++ app/api/config.js
@@ -1,7 +1,8 @@
-export const BASE_URL = 'https://finalbeck.nomorepartiesco.ru/admin/dashboard'
+export const BASE_URL = 'https://finalbeck.nomorepartiesco.ru/api'
+
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
-  auth: `${BASE_URL}/auth/local`,
-  me: `${BASE_URL}/users/me`,
+  auth: `${BASE_URL}/auth/login`,
+  me: `${BASE_URL}/me`,
 }
